2c0d046f75a21ff253ca107a2663b595f6dccd3a
NemissU <namphse171651@fpt.edu.vn>
2024-07-23 00:03:56 +0700

    cache

--- JewelryStorePRN221/obj/Debug/net8.0/JewelryStorePRN221.AssemblyInfo.cs
+++ JewelryStorePRN221/obj/Debug/net8.0/JewelryStorePRN221.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JewelryStorePRN221")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c2d1726c8d6244cfeffbbddd9a2372c449730db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8036970098d8810cd30df02923810bc53dcee4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("JewelryStorePRN221")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JewelryStorePRN221")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
